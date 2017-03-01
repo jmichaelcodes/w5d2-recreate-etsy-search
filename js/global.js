7549ca95d030c7be66fe981a6590f1a233f6a150
Julius Williams <doctorj89@gmail.com>
2017-03-01 15:17:52 -0500

    etsy data

--- js/global.js
+++ js/global.js
@@ -1,47 +1,45 @@
-createCard({
-    src: 'http://unsplash.it/400/300?image=100',
-    caption: 'Item Name',
-    seller: 'Seller',
-    price: '$$$.$$'
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=200',
-    caption: 'Item Name',
-    seller: 'Seller',
-    price: '$$$.$$'
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=300',
-    caption: 'Item Name',
-    seller: 'Seller',
-    price: '$$$.$$'
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=400',
-    caption: 'Item Name',
-    seller: 'Seller',
-    price: '$$$.$$'
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=500',
-    caption: 'Item Name',
-    seller: 'Seller',
-    price: '$$$.$$'
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=600',
-    caption: 'Item Name',
-    seller: 'Seller',
-    price: '$$$.$$'
-});
+var items = [];
 
-function createCard(image) {
+getItems();
+
+// createCard({
+//     src: items[0],
+//     title: items[0],
+//     seller: items[0],
+//     price: items[0]
+// });
+
+
+function getItems() {
+    fetch('https://thinksaydo.com/tiyproxy.php?https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent('board games') + '&includes=Images,Shop')
+        .then(response => response.json())
+        .then(data => {
+            items = data.results;
+        })
+
+        .then(whatever => console.log(items[0]))
+        .then(whatever => renderUserCards());
+
+}
+
+function renderUserCards() {
+    items.forEach(function(user) {
+
+        createCard(user);
+
+    })
+}
+
+function createCard(items) {
     var card = `<div class="card">
-            <img src="${image.src}" alt="Image"/>
-            <h5 class="card-caption">${image.caption}</h5>
-            <h5 class="col-sm-6 card-caption seller">${image.seller}</h5>
-            <h5 class="col-sm-6 card-caption price">${image.price}</h5>
+            <img class="cardImage" src="${items.Images[0].url_75x75}" alt="Image"/>
+            <h5 class="card-caption">${items.title}</h5>
+            <h5 class="col-sm-6 card-caption seller">${items.Shop.shop_name}</h5>
+            <h5 class="col-sm-6 card-caption price">${items.price}</h5>
         </div>`;
+
+
+
 
     document.querySelector('#app').innerHTML += card;
 }
